4078323b104d84ff52c591a4b66ddd76f5a75e45
Mickael Lagattu <mickael.lagattu18@gmail.com>
2019-02-25 16:35:54 +0100

    calcul des coups suivants

---
+++ python/constraint_programming.py
@@ -0,0 +1,198 @@
+#!/usr/bin/env python3
+
+# 2018 - Ecole Centrale Supélec 
+# Les élèves du cours d'optimisation et leur enseignant Christoph Dürr
+# version 2
+
+
+import sys
+    
+class constraint_program:
+    """Implements a basic constraint programming solver with binary constraints.
+
+    Variables names can be integers, strings, any hashable object.
+
+    Variable i has value assign[i] in a solution. While a solution is begin
+    searched, some variables are not yet assigned, which is indicated by
+    assign[i] == None.
+
+    Constraints over two variables (aka binary constraints) are stored in the
+    attribute constr. For every variable x, constr[x] contains a list of
+    couples (y, rel), where y another variable and rel is the set of value
+    pairs (u,v) that are valid in the contraint relating x with y, i.e. x=u
+    and y=v is valid for this constraint.
+    """
+
+    def __init__(self, var):
+        """creates an instance to the constraint programming problem.
+
+        :param var: a dictionary that associates to each variable x, 
+                    its domain var[x].  
+                    The domain can be a list or a set.
+        """
+        self.var = var
+        self.constr = {x: [] for x in var}
+        self.assign = {x: None for x in var}
+        self.log = []  # history of domain changes
+        self.maintain_arc_consistency = False
+        self.stat_nodes = 0
+        # sanity check
+        for x in var:
+            for y in var:
+                if x != y and var[x] is var[y]:
+                    raise "variables have same domain object"
+
+    def add_constraint(self, x, y, rel):
+        """Adds a binary constraint over the variables x and y.
+
+        :param x,y: indices of variables
+        :param rel: set of couples of values defining the relation
+        :comment: it is ok to add several constraints with the same 
+            relation object.
+        """
+        self.constr[x].append((y, rel))
+        self.constr[y].append((x, {(v,u) for (u,v) in rel}))
+
+    def solve_all(self):
+        """Iterates over all solutions
+        """
+        self.stat_nodes += 1
+        x = self.choice_var()  # branching variable 
+        if x is None:
+            yield self.assign  # solution found
+        else:
+            for u in self.var[x]:
+                # if not self.backward_check(x, u):
+                #     continue
+                history = self.save_context()
+                self.assign[x] = u         # try all possible assignements to variable x
+                Q = self.forward_check(x)
+                if self.maintain_arc_consistency:
+                    self.arc_consistency(Q)
+                for sol in self.solve_all():
+                    yield sol
+                self.assign[x] = None
+                self.restore_context(history)
+
+    def solve(self):
+        """Finds one solution
+        """
+        for sol in self.solve_all():
+            return sol
+        return None
+
+    def choice_var(self):
+        """Choose a branching variable 
+
+        :returns: a variable index x such that assign[x] == None and its domain is minimal
+        """
+        best = None
+        for x in self.var:
+            if self.assign[x] is None and \
+                (best is None or len(self.var[x]) < len(self.var[best])):
+                best = x
+        return best
+
+    def backward_check(self, x, u):
+        for (y, rel) in self.constr[x]:
+            v = self.assign[y]
+            if v is not None and (u, v) not in rel:
+                # print("bc {}={}, {}={} not in {}".format(x, u, y, v, str(rel)))
+                return False
+        return True
+
+    def forward_check(self, x): 
+        """After x has been assigned to some value u, 
+            remove recursively from all related variables y values v  
+            such that (u,v) is not in the relation 
+            relating x with y.
+
+        :param x: a variable index
+        :param v: a value in the domain of x
+        :returns: the set of variables for which the domain changed.
+        """
+        changed = set()
+        for (y, rel) in self.constr[x]:
+            if self.assign[y] is None:
+                to_remove = set()
+                for v in self.var[y]:
+                    if (self.assign[x], v) not in rel:
+                        to_remove.add(v)
+                if to_remove:
+                    self.remove_vals(y, to_remove)
+                    changed.add(y)
+        return changed
+
+
+    def set_arc_consistency(self):
+        """Sets the solver in a mode where it maintains 
+           arc_consistency at every moment.
+        """
+        self.maintain_arc_consistency = True
+        all_vars = set(self.var.keys())
+        self.arc_consistency(all_vars)
+
+
+    def arc_consistency(self, Q):
+        """Maintains the arc consistency after the domain of 
+        the variables in set Q got decreased.
+
+        Implements the algorithm AC3.
+
+        :param Q: a set of variable indices
+        """
+        while Q:
+            x = Q.pop()
+            for (y, relation) in self.constr[x]:
+                if self.assign[y] is None:
+                    if self.revise(x, y, relation):
+                        Q.add(y)
+
+
+    def revise(self, x, y, relation): 
+        """The domain of the variable x just got
+           decreased. This method checks if the domain of y 
+           contains values v which do not have support 
+           in the domain of x for the given relation,
+           in which case it removes them.
+
+        :returns: True if the domain of y has been changed.
+        """
+        to_remove = set()
+        for v in self.var[y]:
+            if not self.hasSupport(y, v, x, relation):
+                to_remove.add(v)
+        self.remove_vals(y, to_remove)
+        return to_remove
+
+    def hasSupport(self, y, v, x, relation):
+        """Checks whether the assignment y=v has a supporting 
+           value u in the domain of x, i.e.
+           such that (u,v) belongs to the given relation.
+        """
+        for u in self.var[x]:
+            if (u, v) in relation:
+                    return True
+        return False
+
+    def remove_vals(self, y, to_remove):
+        """ Removes values from the domain of y
+        """
+        self.var[y] -= to_remove # set difference
+        self.log.append((y, to_remove))
+
+    def save_context(self):
+        """ Returns an index which can be used to restore the 
+            variable domains up to this point.
+        """
+        return len(self.log)
+
+    def restore_context(self, history):
+        """ Restores the domains of the variables 
+            up to the given point in time.
+        """
+        while len(self.log) > history:
+            (y, to_remove) = self.log.pop()
+            self.var[y] |= to_remove  # set union
+
+
---
+++ python/possible_plays.py
@@ -0,0 +1,176 @@
+from constraint_programming import constraint_program
+from action import Action
+from scipy.special import binom
+
+
+class PossiblePlays:
+    """Classe qui permet de déterminer les coups possibles depuis un état donné"""
+
+    @staticmethod
+    def get_possible_plays(state, m, n, player=0):
+        """player : 0 pour V, 1 pour W
+        m et n sont la taille de la grille
+        (m coordonnée en x et m en y)"""
+
+        j = "V" if player==0 else "W"
+        var = {}
+        for entity in state:
+            if entity["type"] == j:
+                available_cases = []
+                for k in[-1, 0, 1]:
+                    for l in [-1, 0, 1]:
+                        if (0 <= entity['x'] + k < m) and (0<= entity['y'] + l <= n):
+                            available_cases.append((entity['x'] + k, entity['y'] + l))
+                for individual in range(entity["number"]):
+                    var[(entity["type"], individual, entity["x"], entity["y"])] = available_cases
+
+        P = constraint_program(var)
+        P.set_arc_consistency()
+        solutions = P.solve_all()
+
+        movements = {}
+        for solution in solutions:
+            for (_, _, x_init, y_init), (x_end, y_end) in solution.items():
+                if x_init != x_end or y_init != y_end:
+                    if (x_init, y_init, x_end, y_end) not in movements:
+                        movements[((x_init, y_init, x_end, y_end))] = 1
+                    else:
+                        movements[((x_init, y_init, x_end, y_end))] += 1
+            if len(movements) > 0:
+                action = Action()
+                for (x_init, y_init, x_end, y_end), number in movements.items():
+                    action.add_deplacement(j, number, (x_init, y_init), (x_end, y_end))
+                yield action
+
+    @staticmethod
+    def get_next_states(state, action):
+        """Calculates the possible next states given a previous state and an action"""
+        # On cherche les batailles aléatoires
+        movements = action.get_deplacements()
+        states_pile = [(state, 1, 0)] #Etat, proba, étape
+        while len(states_pile) > 0:
+            current_state, proba, step = states_pile.pop()
+            if step == len(movements):
+                yield current_state, proba
+            else:
+                _, number, (x_init, y_init), (x_end, y_end) = movements[step]
+                entity1 = PossiblePlays.what_in_that_case(current_state, x_init, y_init)
+                entity2 = PossiblePlays.what_in_that_case(current_state, x_end, y_end)
+                if entity2 is None:
+                    if entity1["number"] > number:
+                        current_state = PossiblePlays.set_case(current_state, x_init, y_init, {"type":entity1["type"], "number":entity1["number"] - number, "x":x_init, "y":y_init})
+                        current_state = PossiblePlays.set_case(current_state, x_end, y_end, {"type":entity1["type"], "number":number, "x":x_end, "y":y_end})
+                    else:
+                        current_state = PossiblePlays.remove_case(current_state, x_init, y_init)
+                        current_state = PossiblePlays.set_case(current_state, x_end, y_end,
+                                                               {"type": entity1["type"], "number": number, "x": x_end,
+                                                                "y": y_end})
+                    states_pile.append((current_state, proba, step+1))
+
+                elif entity2["type"] == entity1["type"]:
+                    if entity1["number"] > number:
+                        current_state = PossiblePlays.set_case(current_state, x_init, y_init, {"type":entity1["type"], "number":entity1["number"] - number, "x":x_init, "y":y_init})
+                        current_state = PossiblePlays.set_case(current_state, x_end, y_end, {"type":entity1["type"], "number":number + entity2["number"], "x":x_end, "y":y_end})
+                    else:
+                        current_state = PossiblePlays.remove_case(current_state, x_init, y_init)
+                        current_state = PossiblePlays.set_case(current_state, x_end, y_end,
+                                                               {"type": entity1["type"], "number": number + entity2["number"], "x": x_end,
+                                                                "y": y_end})
+                        states_pile.append((current_state, proba, step + 1))
+
+                elif entity2["type"] == "H" and entity1["number"] >= entity2["number"]:
+                    if entity1["number"] > number:
+                        current_state = PossiblePlays.set_case(current_state, x_init, y_init, {"type":entity1["type"], "number":entity1["number"] - number, "x":x_init, "y":y_init})
+                        current_state = PossiblePlays.set_case(current_state, x_end, y_end, {"type":entity1["type"], "number":number + entity2["number"], "x":x_end, "y":y_end})
+                    else:
+                        current_state = PossiblePlays.remove_case(current_state, x_init, y_init)
+                        current_state = PossiblePlays.set_case(current_state, x_end, y_end,
+                                                               {"type": entity1["type"], "number": number + entity2["number"], "x": x_end,
+                                                                "y": y_end})
+                        states_pile.append((current_state, proba, step + 1))
+
+                elif entity2["type"] == "H":
+                    #Il y a bataille joueur-humain
+                    #on commence par vider la case de départ
+                    if entity1["number"] > number:
+                        current_state = PossiblePlays.set_case(current_state, x_init, y_init, {"type":entity1["type"], "number":entity1["number"] - number, "x":x_init, "y":y_init})
+                    else:
+                        current_state = PossiblePlays.remove_case(current_state, x_init, y_init)
+                    P_victory = number / (2*entity2["number"])
+                    #Ensemble des issues possibles
+                    #Issue avec 0 (on perd ou tout le mond meurt):
+                    current_state = PossiblePlays.remove_case(current_state, x_end, y_end)
+                    new_proba =(1-P_victory)**(entity2["number"] + number) * P_victory + (1-P_victory)
+                    states_pile.append((current_state, proba * new_proba, step + 1))
+
+                    for possibility in range(1, number + entity2["number"]+1):
+                        current_state = PossiblePlays.set_case(current_state, x_end, y_end, {"type": entity1["type"],
+                                                                                             "number": possibility, "x": x_end,
+                                                                                             "y": y_end})
+                        new_proba = P_victory**(possibility+1) * (1-P_victory)**(entity2["number"] + number - possibility) * binom(entity2["number"] + number, possibility)
+                        states_pile.append((current_state, proba * new_proba, step + 1))
+
+                elif number >= 1.5 * entity2["number"]:
+                    # On arrive sur des ennemis et on les défonce
+                    if entity1["number"] > number:
+                        current_state = PossiblePlays.set_case(current_state, x_init, y_init, {"type":entity1["type"], "number":entity1["number"] - number, "x":x_init, "y":y_init})
+                    else:
+                        current_state = PossiblePlays.remove_case(current_state, x_init, y_init)
+                    current_state = PossiblePlays.set_case(current_state, x_end, y_end, {"type": entity1["type"],
+                                                                                         "number": number,
+                                                                                         "x": x_end,
+                                                                                         "y": y_end})
+                    states_pile.append((current_state, proba, step + 1))
+                else:
+                    #Bataille contre ennemis
+                    if entity1["number"] > number:
+                        current_state = PossiblePlays.set_case(current_state, x_init, y_init, {"type":entity1["type"], "number":entity1["number"] - number, "x":x_init, "y":y_init})
+                    else:
+                        current_state = PossiblePlays.remove_case(current_state, x_init, y_init)
+                if number >=  entity2["number"]:
+                    P_victory = (number/entity2["number"] - 0.5)
+                else:
+                    P_victory = number/(2*entity2["number"])
+                    # Ensemble des issues possibles
+                    # Issue avec 0 (on perd ou tout le mond meurt):
+                current_state = PossiblePlays.remove_case(current_state, x_end, y_end)
+                new_proba = (1 - P_victory) ** number * P_victory + (1-P_victory)
+                states_pile.append((current_state, proba * new_proba, step + 1))
+
+                for possibility in range(1, number+1):
+                    current_state = PossiblePlays.set_case(current_state, x_end, y_end, {"type": entity1["type"],
+                                                                                         "number": possibility,
+                                                                                         "x": x_end,
+                                                                                         "y": y_end})
+                    new_proba = P_victory ** (possibility + 1) * (1 - P_victory) ** (
+                            number - possibility) * binom(number,
+                                                                                  possibility)
+                    states_pile.append((current_state, proba * new_proba, step + 1))
+
+
+
+
+
+
+    @staticmethod
+    def what_in_that_case(state, x, y):
+        for entity in state:
+            if entity["x"] == x and entity["y"] == y:
+                return entity
+        return None
+
+    @staticmethod
+    def remove_case(state, x, y):
+        state = [entity for entity in state if not (entity["x"] == x and entity["y"] == y)]
+        return state
+
+    @staticmethod
+    def set_case(state, x, y, new_case):
+        state = [entity for entity in state if not (entity["x"]== x and entity["y"]== y)]
+        state.append(new_case)
+        return state
+
+
+
+
+
