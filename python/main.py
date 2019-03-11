f0b0655a6578bfa8886b1c75db3b1e16d2ad0682
MickaelLagattu <mickael.lagattu18@gmail.com>
2019-03-11 16:07:47 +0100

    ça marche

--- python/main.py
+++ python/main.py
@@ -1,51 +1,60 @@
 import argparse
 import socket
-
+import sys
+from alpha_beta import AlphaBeta
 try:
-    from tcpServer import werewolf_player, vampire_player
     from tcpServer.echo_client import *
     import state
 
 except:
-    from python.tcpServer import werewolf_player, vampire_player
     from python.tcpServer.echo_client import *
     from python import state
 
 def generic_player(host, port):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.connect((host, port))
-        send_nme_command(s, 'Equipe4')
+        send_nme_command(s, 'MECHANTSLOUPS')
+        alphabeta = AlphaBeta()
         game = None
-        command = read_command(s)
-        print(command)
+        role = None
+        while True:
+            command = read_command(s)
+            print(command)
+            if command == 'BYE':
+            # sleep(2)
+                pass
+            elif command == 'END':
+                break
+            if command == 'SET':
+                set = receive_set_command(s)
+                print(set)
+                game = state.State(set[1], set[0])
+            elif command == 'HUM':
+                print(receive_hum_command(s))
+            elif command == 'HME':
+                x,y = receive_hme_command(s)
+                print(x,y)
+            elif command == 'MAP':
+                map = receive_map_command(s)
+                print(map)
+                game.update(map[-1])
+                role = game.findObjectAtLocation(x,y)['type']
+            elif command == 'UPD':
+                upd = receive_upd_command(s)
+                print(upd)
+                game.update(upd[-1])
 
-        if command == 'SET':
-            set = receive_set_command(s)
-            print(set)
-            game = state.State(set[1], set[0])
-        elif command == 'HUM':
-            print(receive_hum_command(s))
-        elif command == 'HME':
-            x,y = receive_hme_command(s)
-            print(x,y)
-            print(game.findObjectAtLocation(x,y)['type'])
-
-        elif command == 'MAP':
-            map = receive_map_command(s)
-            print(map)
-            game.update(map[-1])
-
+                # send_mov_command(s, game.next_move_example_random('V'))
+                send_mov_command(s, alphabeta.get_best_next_action(game, role))
+                # TODO add a function that makes the next move (returns lov_list)
+                # TODO send mov_list
+            else:
+                pass
+                # raise ValueError('Unknown command')
+                
 if __name__ == "__main__":
-    ap = argparse.ArgumentParser()
-    ap.add_argument("-i", "--ipAddress", required=True, type=str, help="The game server ip")
-    ap.add_argument("-p", "--port", required=True, type=str, help="The game server port")
-    ap.add_argument("-r", "--role", required=True, type=str, help="Our player role")
+    ip = sys.argv[1]
+    port = int(sys.argv[2])
 
-    args = vars(ap.parse_args())
-
-    if args['role'] == 'W':
-        generic_player(args['ipAddress'], args['port'])
-    if args['role'] == 'V':
-        vampire_player.vampire_game(args['ipAddress'], args['port'])
-    else:
-        raise ValueError('Unkown player!')
+    generic_player(ip, port)
+    
